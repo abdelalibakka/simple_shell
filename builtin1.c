4158cf7d0636f64dde44a5254bdce04cf47c2192
abdelalibakka <pakkatatata@gmail.com>
2023-05-18 09:52:21 -0700

    update

---
+++ builtin1.c
@@ -0,0 +1,60 @@
+/**
+ * struct builtins - structure for built-in commands
+ * @name: name of the command
+ * @func: function pointer to the command
+ */
+typedef struct builtins
+{
+	char *name;
+	int (*func)(char **args, char **env);
+} builtins_t;
+
+/**
+ * print_env - prints the current environment
+ * @args: the arguments passed to the function
+ * @env: the environment variables
+ *
+ * Return: Always returns 0
+ */
+int print_env(char **args, char **env)
+{
+	int i;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		_puts(env[i]);
+		_putchar('\n');
+	}
+	return (0);
+}
+
+/**
+ * print_exit_error - prints an error message if exit fails
+ * @args: the arguments passed to the function
+ * @env: the environment variables
+ *
+ * Return: Always returns -1
+ */
+int print_exit_error(char **args, char **env)
+{
+	perror(args[0]);
+	return (-1);
+}
+
+/**
+ * exit_shell - exits the shell
+ * @args: the arguments passed to the function
+ * @env: the environment variables
+ *
+ * Return: Always returns -1
+ */
+int exit_shell(char **args, char **env)
+{
+	int exit_status = 0;
+
+	if (args[1] != NULL)
+	{
+		exit_status = _atoi(args[1]);
+	}
+	exit(exit_status);
+}
