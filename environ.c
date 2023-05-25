92127987d4c2847f72a54cda3c2bdf9b6a243b6a
abdelalibakka <pakkatatata@gmail.com>
2023-05-25 16:00:54 -0700

    update

--- environ.c
+++ environ.c
@@ -13,7 +13,7 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets the value of an environ variable
+ * _getenv - gets the value of an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
  *
@@ -39,13 +39,13 @@ char *_getenv(info_t *info, const char *name)
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- *  Return: Always 0
+ * Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_eputs("Incorrect number of arguments\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -65,10 +65,10 @@ int _myunsetenv(info_t *info)
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		_eputs("Too few arguments.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
+	for (i = 1; i < info->argc; i++)
 		_unsetenv(info, info->argv[i]);
 
 	return (0);
