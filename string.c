20d1dfba0d5464973bb5343f1bd1a82b4f9e4348
abdelalibakka <pakkatatata@gmail.com>
2023-05-24 17:37:02 -0700

    update

---
+++ string.c
@@ -0,0 +1,83 @@
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string whose length to check.
+ *
+ * Return: Integer length of the string.
+ */
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	if (!s)
+		return (0);
+
+	while (s[length])
+		length++;
+
+	return (length);
+}
+
+/**
+ * _strcmp - Performs lexicographic comparison of two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2.
+ */
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
+/**
+ * starts_with - Checks if needle starts with haystack.
+ * @haystack: The string to search.
+ * @needle: The substring to find.
+ *
+ * Return: Address of the next char of haystack or NULL.
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+	{
+		if (*needle != *haystack)
+			return (NULL);
+		needle++;
+		haystack++;
+	}
+
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination buffer.
+ * @src: The source buffer.
+ *
+ * Return: Pointer to the destination buffer.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *ret = dest;
+
+	if (dest == NULL || src == NULL)
+		return (NULL);
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+
+	return (ret);
+}
