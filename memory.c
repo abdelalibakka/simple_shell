cdd433d33dd91ece1202c117f261444698dd599c
abdelalibakka <pakkatatata@gmail.com>
2023-05-26 12:15:09 -0700

    memory.c

--- memory.c
+++ memory.c
@@ -4,15 +4,19 @@
  * bfree - frees a pointer and NULLs the address
  * @ptr: address of the pointer to free
  *
- * Return: 1 if freed, otherwise 0.
+ * This function frees the memory allocated for the pointer and sets the pointer
+ * to NULL.
+ *
+ * Return: 1 if the memory was freed successfully, otherwise 0.
  */
 int bfree(void **ptr)
 {
-	if (ptr && *ptr)
+	if (ptr != NULL && *ptr != NULL)
 	{
 		free(*ptr);
 		*ptr = NULL;
 		return (1);
 	}
+
 	return (0);
 }
